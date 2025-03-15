8cb3786ec61de0c401799b01d46a1452ce5a2664
mario32111 <marioge44@gmail.com>
2025-03-15 11:55:03 -0600

    actalizacion de categorias

--- src/slices/types.ts
+++ src/slices/types.ts
@@ -1,33 +1,44 @@
 //DATOS DE LA API
-export interface Question{
-    id: number;
-    categoryId: number;
-    text: string;
-    image: string | null;
-    category: Category;
-    options: Option[];
-  }
-  
+export interface Question {
+  id: number;
+  categoryId: number;
+  text: string;
+  image: string | null;
+  category: Category;
+  options: Option[];
+}
+
 export interface Option {
-    id: number;
-    questionId: number;
-    text: string;
-    isCorrect: boolean;
-  }
-  
+  id: number;
+  questionId: number;
+  text: string;
+  isCorrect: boolean;
+}
+
 export interface Category {
-    id: number;
-    name: string;
-    usersQuantity: number;
-  }
+  id: number;
+  name: string;
+  usersQuantity: number;
+}
+
+export interface History {
+  redes: boolean;
+  web: boolean;
+  ia: boolean;
+  bd: boolean;
+  seguridad: boolean;
+}
 
 
 export interface DataState {
-    questions: Question[];
-    currentQuestion: Question;
-    selectedOption?: string;
-    score: number;
+  questions: Question[];
+  currentQuestion: Question;
+  selectedOption?: string;
+  score: number;
+  history: History,
 }
+
+
 
 
 
@@ -35,18 +46,17 @@ export interface DataState {
 
 //UI
 export interface UiState {
-    loading: boolean;
-    isCorrect: boolean;
-    progress: number;
-    openedModal: boolean;
-    showScoreModal: boolean;
+  loading: boolean;
+  isCorrect: boolean;
+  progress: number;
+  openedModal: boolean;
+  showScoreModal: boolean;
 }
 
 
 
 export interface RootState {
-    ui: UiState;     // Slice para el estado de la UI
-    data: DataState; // Slice para el estado de los datos
-  }
+  ui: UiState;     // Slice para el estado de la UI
+  data: DataState; // Slice para el estado de los datos
+}
 
-  
