32f14a830b53eedd3d63fd0558ef58449776f3ab
mario32111 <marioge44@gmail.com>
2025-03-19 11:01:14 -0600

    error de modo oscuro

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -225,7 +225,7 @@ const ParticlesBackground = () => {
     }
   }, []);
 
-  return <div id="particles-js" style={{ position: "absolute", width: "100%", height: "96%", zIndex: -1 }}></div>;
+  return <div id="particles-js" style={{ position: "absolute", width: "100%", height: "100%", zIndex: -1, backgroundColor: "black" }}></div>;
 };
 
 export default ParticlesBackground;
